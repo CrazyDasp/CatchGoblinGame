40280108abf9340990f0abc1bd3686ca1e49d3da
Roman <kameta-r3@yandex.ru>
2025-01-19 18:03:59 +0300

    Init

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import './styles.css';
+import goblin from './img/Goblin.png';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const board = document.getElementById('game-board');
+
+  const cells = [];
+  for (let i = 0; i < 16; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    board.appendChild(cell);
+    cells.push(cell);
+  }
+
+  const img = document.createElement('img');
+  img.src = goblin;
+  img.alt = 'Goblin';
+  let currentIndex = Math.floor(Math.random() * cells.length);
+  cells[currentIndex].appendChild(img);
+
+  setInterval(() => {
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * cells.length);
+    } while (newIndex === currentIndex);
+    cells[newIndex].appendChild(img);
+    currentIndex = newIndex;
+  }, 1000);
+});
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new ESLintPlugin(),
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+    hot: true,
+  },
+};
